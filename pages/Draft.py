2d2cc7e022b4cd668fac5b557a8a7da24ac33624
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-15 17:28:16 +0200

    Update Draft.py

--- pages/Draft.py
+++ pages/Draft.py
@@ -9,12 +9,11 @@ df_Draft['Points']=df_Draft['Points'].replace({'-':'0'}).astype(int)
 
 Year = ['All Seasons'] + list(df_Draft['Year'].unique())
 Positions = ['All'] + list(df_Draft['Position'].unique())
-
-Picks=df_Draft.Pick.unique()
-Franchise=df_Draft.Franchise.unique()
+Franchise = ['All'] + list(df_Draft['Franchise'].unique())
 
 position_choice_year = st.radio('Year:', Year,horizontal=True)
 selected_position = st.selectbox('Position', Positions, index=0)
+selected_franchise = st.selectbox('Franchise', Positions, index=0)
 
 
 if position_choice_year != 'All Seasons':
@@ -24,6 +23,9 @@ else:
 
 if selected_position != 'All':
     df_filtered = df_filtered[df_filtered['Position'] == selected_position]
+
+if selected_franchise != 'All':
+    df_filtered = df_filtered[df_filtered['Franchise'] == selected_position]
 
 # Mostrar la tabla en Streamlit
 st.dataframe(df_filtered)
