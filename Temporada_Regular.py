dbdf78dc2a4757f8a3ef69c628cdb0449191d6df
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-15 13:58:52 +0200

    Add files via upload

--- Temporada_Regular.py
+++ Temporada_Regular.py
@@ -22,21 +22,22 @@ df_clasification=df_clasification[['Team','Record','%W','GB','STRK','PF','AVG_PF
                                                 'AVG_PA','DIV_Record','CON_Record']]
 
 # FRANCHISE
-Don_Shula = ['Isma Ray Ravens', "Jrz French Faulk's", 'Darko LaDainian Texas *', 'Zgz Marino Phins',
-             "Mile High Champ's"]
-Larry_Csonka = ['Javier Big Ben Steelers', 'GronkSpike', "The Adam´s Family", 'San Joe Montana 49ers']
+Vince_Lombardi = ['Isma Ray Ravens', "Jrz French Faulk's", 'Darko LaDainian Texas *', 'Zgz Marino Phins',
+             "Mile High Champ's",'Snake Stabler Raiders','Medina Allen Bills']
+Brett_Favre = ['Javier Big Ben Steelers', 'GronkSpike', "The Adam´s Family", 'San Joe Montana 49ers'
+               ,'Cowher Straight Outta Albacete']
 Bruce_Smith = ['Franquicia Sin Nombre', 'Triana Bills Kelly', 'NavaWarner Rams', 'Indiana James',
                'El Predicador Almogávar', 'Pittsburgh Terror Lambert', 'Nafarroa Bills Kelly']
 Marv_Levy = ["Smith's Terrible Showdown", 'Malaka Reed Poe', 'Córdoba Gunslingers Favre', "Vlc Tigers Watt's"]
 
 # PRINT
-st.subheader("CONFERENCIA DAN MARINO - DIVISIÓN DON SHULA")
-df_clasification_Shula = df_clasification[df_clasification['Team'].isin(Don_Shula)]
+st.subheader("CONFERENCIA REGGIE WHITE - DIVISIÓN VINCE LOMBARDI")
+df_clasification_Shula = df_clasification[df_clasification['Team'].isin(Vince_Lombardi)]
 df_clasification_Shula = df_clasification_Shula.set_index('Team')
 st.dataframe(df_clasification_Shula.style.format({'%W': '{:.1f}', 'AVG_PF': '{:.1f}', 'AVG_PA': '{:.1f}'}))
 
-st.subheader("CONFERENCIA DAN MARINO - DIVISIÓN LARRY CSONKA")
-df_clasification_Csonka = df_clasification[df_clasification['Team'].isin(Larry_Csonka)]
+st.subheader("CONFERENCIA REGGIE WHITE - DIVISIÓN BRETT FAVRE")
+df_clasification_Csonka = df_clasification[df_clasification['Team'].isin(Brett_Favre)]
 df_clasification_Csonka = df_clasification_Csonka.set_index('Team')
 st.dataframe(df_clasification_Csonka.style.format({'%W': '{:.1f}', 'AVG_PF': '{:.1f}', 'AVG_PA': '{:.1f}'}))
 
