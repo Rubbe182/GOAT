059f072bb8bbe7c780e7fb5a5e1332548d91eb33
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-15 17:11:59 +0200

    Update Draft.py

--- pages/Draft.py
+++ pages/Draft.py
@@ -4,14 +4,16 @@ import streamlit as st
 df_Draft = pd.read_csv('Draft.csv',encoding='latin-1')
 st.title("DRAFT")
 
+
+
+Year=['All Seasons',2019,2020,2021,2022]
+Picks=df_Draft.Pick.unique()
+Franchise=df_Draft.Franchise.unique()
+Position=df_Draft.Position.unique()
+
+position_choice_year = st.radio('Year:', Year,horizontal=True)
+position_choice_Picks = st.selectbox('Picks:', Picks)
+position_choice_Franchise = st.multiselect('Franchise:', Franchise)
+
 # Mostrar la tabla en Streamlit
 st.dataframe(df_Draft)
-
-# Agregar filtros para cada columna
-for col in df_Draft.columns:
-    filter_values = df_Draft[col].unique()
-    selected_values = st.multiselect(f'Filtrar por {col}', filter_values)
-    filtered_df = df_Draft[df_Draft[col].isin(selected_values)]
-    
-    # Mostrar el resultado filtrado
-    st.dataframe(filtered_df)
