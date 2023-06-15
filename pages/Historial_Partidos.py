b05795794ba602f61693ce04ba61526198e4dd89
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-15 15:09:35 +0200

    Add files via upload

--- pages/Historial_Partidos.py
+++ pages/Historial_Partidos.py
@@ -8,10 +8,10 @@ df_Games = pd.read_csv('df_Games.csv')
 st.title("Historial Partidos")
 st.sidebar.markdown('Historial Partidos Temporada Regular')
 df_Games=df_Games[['Year','Week','Home_Team','Away_Team','Home_Points','Away_Points']]
-Weeks=['1','2','3','4','5','6','7','8','9','10','11','12','13']
+Weeks=['1','2','3','4','5','6','7','8','9','10','11','12','13','14']
 df_Games=df_Games[df_Games.Week.isin(Weeks)]
 
-Week=['All Weeks','1','2','3','4','5','6','7','8','9','10','11','12','13']
+Week=['All Weeks','1','2','3','4','5','6','7','8','9','10','11','12','13','14']
 Year=['All Seasons',2019,2020,2021,2022]
 Teams=df_Games.Home_Team.unique()
 
@@ -27,23 +27,23 @@ if position_choice_display=='Selección Franquicia':
     if position_choice_year=='All Seasons' and position_choice_week=='All Weeks':
         df_Games = df_Games.loc[(df_Games['Home_Team'].isin(position_choice_Team)) |
                                 (df_Games['Away_Team'].isin(position_choice_Team))]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_week=='All Weeks':
         df_Games = df_Games.loc[(df_Games['Home_Team'].isin(position_choice_Team)) |
                                 (df_Games['Away_Team'].isin(position_choice_Team))]
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_year=='All Seasons':
         df_Games = df_Games.loc[(df_Games['Home_Team'].isin(position_choice_Team)) |
                                 (df_Games['Away_Team'].isin(position_choice_Team))]
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     else:
         df_Games = df_Games.loc[(df_Games['Home_Team'].isin(position_choice_Team)) |
                                 (df_Games['Away_Team'].isin(position_choice_Team))]
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
 
 elif position_choice_display=='Head to Head':
     position_choice_Team1 = st.selectbox('Team 1:', Teams)
@@ -53,21 +53,21 @@ elif position_choice_display=='Head to Head':
                                 (df_Games['Away_Team']==(position_choice_Team2))|
                                 (df_Games['Away_Team']==(position_choice_Team1))&
                                 (df_Games['Away_Team']==(position_choice_Team2))]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_week=='All Weeks':
         df_Games = df_Games.loc[(df_Games['Home_Team'] == (position_choice_Team1)) &
                                 (df_Games['Away_Team'] == (position_choice_Team2)) |
                                 (df_Games['Away_Team'] == (position_choice_Team1)) &
                                 (df_Games['Away_Team'] == (position_choice_Team2))]
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_year=='All Seasons':
         df_Games = df_Games.loc[(df_Games['Home_Team'] == (position_choice_Team1)) &
                                 (df_Games['Away_Team'] == (position_choice_Team2)) |
                                 (df_Games['Away_Team'] == (position_choice_Team1)) &
                                 (df_Games['Away_Team'] == (position_choice_Team2))]
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     else:
         df_Games = df_Games.loc[(df_Games['Home_Team'] == (position_choice_Team1)) &
                                 (df_Games['Away_Team'] == (position_choice_Team2)) |
@@ -75,18 +75,18 @@ elif position_choice_display=='Head to Head':
                                 (df_Games['Away_Team'] == (position_choice_Team2))]
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
 
 else:
     if position_choice_year=='All Seasons' and position_choice_week=='All Weeks':
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_week=='All Weeks':
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     elif position_choice_year=='All Seasons':
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
     else:
         df_Games = df_Games[df_Games['Year']==(position_choice_year)]
         df_Games = df_Games[df_Games['Week']==(position_choice_week)]
-        st.table(df_Games)
+        st.dataframe(df_Games)
