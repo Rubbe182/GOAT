717e08683070fa6250b75bc14e30f2ab26586cbb
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-15 16:59:08 +0200

    Add files via upload

---
+++ pages/Draft.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+df_Draft = pd.read_csv('Draft.csv')
+st.title("DRAFT")
+
+# Mostrar la tabla en Streamlit
+st.dataframe(df_Draft)
+
+# Agregar filtros para cada columna
+for col in df_Draft.columns:
+    filter_values = df_Draft[col].unique()
+    selected_values = st.multiselect(f'Filtrar por {col}', filter_values)
+    filtered_df = df_Draft[df_Draft[col].isin(selected_values)]
+    
+    # Mostrar el resultado filtrado
+    st.dataframe(filtered_df)
