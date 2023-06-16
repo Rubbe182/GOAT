cca345d26190c0637b5c9d216ec7f6389824734d
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-06-16 08:41:07 +0200

    Update 3.Draft.py

--- pages/3.Draft.py
+++ pages/3.Draft.py
@@ -9,21 +9,27 @@ df_Draft['Points']=df_Draft['Points'].replace({'-':'0'}).astype(int)
 
 Year = ['All Seasons'] + list(df_Draft['Year'].unique())
 Positions = ['All'] + list(df_Draft['Position'].unique())
-
-Picks=df_Draft.Pick.unique()
-Franchise=df_Draft.Franchise.unique()
+Franchise = ['All'] + list(df_Draft['Franchise'].unique())
 
 position_choice_year = st.radio('Year:', Year,horizontal=True)
 selected_position = st.selectbox('Position', Positions, index=0)
+selected_franchise = st.selectbox('Franchise', Franchise, index=0)
+player_search = st.text_input('Buscar Jugador:', '')
 
-
-if position_choice_year != 'Todos los años':
+if position_choice_year != 'All Seasons':
     df_filtered = df_Draft[df_Draft['Year'] == position_choice_year]
 else:
     df_filtered = df_Draft
-    
-if selected_position != 'Todas las posiciones':
+
+if selected_position != 'All':
     df_filtered = df_filtered[df_filtered['Position'] == selected_position]
+
+if selected_franchise != 'All':
+    df_filtered = df_filtered[df_filtered['Franchise'] == selected_franchise]
+    
+if player_search:
+    df_filtered = df_filtered[df_filtered['Player'].str.contains(player_search, case=False)]
+
 
 # Mostrar la tabla en Streamlit
 st.dataframe(df_filtered)
